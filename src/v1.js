2e657e91b39acd25963a8933e7f9f1b186c4bdbb
Remigiusz Orlowski <remigiusz.orlowski@gmail.com>
2021-03-06 12:33:03 +0100

    🎉 Init project structure

---
+++ src/v1.js
@@ -0,0 +1,14 @@
+/*
+ * The function accepts a STRING `payloadStream` as parameter.
+ *
+ *
+ * The function is expected to RETURN an ARRAY in the following format:
+ *
+ * [{ imei: STRING, batteryLevel: STRING, odometer: STRING, time: DATE },... ]
+ *
+ */
+function getDeviceInformations(payloadStream) {
+  // code here...
+}
+
+module.exports = getDeviceInformations;
---
+++ src/v2.js
@@ -0,0 +1,11 @@
+/*
+ * The function accepts a STRING `payloadStream` as parameter.
+ *
+ * The function is expected to RETURN an ARRAY of Objects. Please refer to
+Example for additional informations.
+ */
+function getDeviceInformations(payloadStream) {
+  // Please copy your code from PART I.
+}
+
+module.exports = getDeviceInformations;
